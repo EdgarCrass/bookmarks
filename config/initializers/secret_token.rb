ce7f17042de753aaa8bdcd6561ac3d7e552189de
Edgar Crass Jr <EJCRASS1@madisoncollege.edu>
2014-03-10 21:07:51 -0500

    rails new bookmarks --database postgresql --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookmarks::Application.config.secret_key_base = 'b423feb14298063f660c5333ac3de9d2adcafddf6da89f5d8ff58053788243fbf907948d42451f07953fe8e6d040300ce94541a7b040dc91b1e26bb5dddd310b'
